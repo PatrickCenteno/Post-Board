d0fc4e862c66d6937a2ae033265e762da0fb5ec9
Patrick Centeno <pcenteno3492@gmail.com>
2016-08-03 15:08:40 -0400

    Converted index to php and added a db constants file

---
+++ postboard/scripts/defines.php
@@ -0,0 +1,11 @@
+<?php 
+	/** 
+	 * Database constant defines
+	 */
+
+	define('DB_H', 'localhost');
+	define('DB_U', 'root');
+	define('DB_P', 'root');
+	define('DB_SCH', 'postboard');
+	define('DB_DSN', 'mysql:host=localhost;dbname=postboard;charset=utf8');
+ ?>
