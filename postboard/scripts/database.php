7de327971c3d147031ff069a4620a236d6a34525
Patrick Centeno <pcenteno3492@gmail.com>
2016-08-03 15:32:15 -0400

    Added database file (for pdos) and init and submit post files that still need to be finished

---
+++ postboard/scripts/database.php
@@ -0,0 +1,11 @@
+<?php 
+	/**
+	 * All PDO functions go in here
+	 */
+	include_once ('defines.php');
+
+	function add_posts($post_text, $date){
+		// TODO
+		return true;
+	}
+ ?>
---
+++ postboard/scripts/submit_post.php
@@ -0,0 +1,24 @@
+<?php 
+	include_once('database.php');
+
+	// Check that the date and the text from the post were sent as POST methods
+	if (!isset($_POST['post_text']);){
+		print_r(json_encode('success' => 'false', 'message' => 'post body empty'));
+		die();
+	}
+
+	if (!isset($_POST['date']);){
+		print_r(json_encode('success' => 'false', 'message' => 'post date empty'));
+		die();
+	}
+
+	// Get post and date values
+	$post_text = $_POST['post_text'];
+	$date = $_POST['date'];
+
+	if (add_posts($post_text, $date))
+		print_r(json_encode('success' => 'true', 'message' => 'posted');
+	else
+		print_r(json_encode('success' => 'true', 'message' => 'post_failed');
+
+ ?>
